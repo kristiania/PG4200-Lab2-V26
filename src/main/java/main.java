db5586f30ed3360eefb86c1f68de7d06fd89e70b
frdo004 <frederic.b.dorn@gmail.com>
2026-01-09 16:51:02 +0100

    Utkast lab 2 practice

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,21 +1,6 @@
-import assignment.Search;
+
 
 void main() {
-    int[] arr = {10, 7, 11, 5, 13, 8, 38, 37, 14, 92, 84, 74, 77, 20, 40, 47, 33, 65, 62, 69, 73};
-    int[] targets = {10,84,73,120};
-    Search ls = new Search(arr);
 
-    for(int elem : targets) {
-
-        int result = ls.linearSearch(elem);
-
-        if (result > -1) {
-            IO.println(String.format("Item found at posistion %d", result));
-        } else {
-            IO.println("Element not found in list.");
-        }
-        int steps = ls.stepsLinearSearch(elem);
-        IO.println(String.format("Linear Search took %d steps to complete.", steps));
-    }
 
 }
